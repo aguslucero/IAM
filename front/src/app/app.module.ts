7a61185568aaaa49b34cd640f8ef1f9151db6cb8
unknown <aguslucero@hotmail.com>
2019-12-18 14:47:19 -0600

    front

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
